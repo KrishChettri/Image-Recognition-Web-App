19a81010f4154bec87ee0ffa2f1beb7c936247c2
KrishChettri <140698645+KrishChettri@users.noreply.github.com>
2024-03-28 20:19:03 +1300

    Add files via upload

--- main.js
+++ main.js
@@ -11,12 +11,29 @@ Webcam.attach(camera);
 
 function take_snapshot(){
     Webcam.snap(function(data_uri) {
-        document.getElementById("result").innerHTML = '<img id="captured_image" src=">'+data_uri+'"';
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'" >';
     });
 }
 console.log('ml5 version', ml5.version);
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/rJSSMisZP/model.json',modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/YpVHLKDn6y/model.json',modelLoaded);
 
 function modelLoaded(){
-    console.log("model is loaded");
+ console.log("model is loaded");
+}
+
+function check()
+{
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results){
+    if (error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("obj_name").innerHTML = results[0].label; 
+        document.getElementById("obj_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
 }
